6feabf137752e136862a94921daf34b0b1f65d6a
RandyJCodes <Randalljackson20@hotmail.com>
2021-06-27 16:04:08 -0400

    Javascript in HTML
    Created a simple Javascript file to work with a basic HTML page.

---
+++ Basic-Javascript-Projects/Project1-Expressions-Alert/JS/main.js
@@ -0,0 +1,8 @@
+var A = "These strings are", B=" created", C=" for the sake of", 
+D=" creating multiple variables at once", E=".", A=A.fontcolor("blue"), 
+B=B.fontcolor("green"), C=C.fontcolor("red"), D=D.fontcolor("violet"),
+E=E.fontcolor("orange") //These variables contain words and their corresponding font colors. 
+document.write(A+B+C+D+E) //This will write out the variables set and it contains a message in rainbow colors. 
+
+var F = "Concatenated" + "together" //This Variable will concatenate the strings. 
+window.alert(F) // This will create an alert box with the Concatenated strings for Variable F. 
